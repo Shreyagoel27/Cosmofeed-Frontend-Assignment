051fedbfdc31f9fe7710ca37c0b77d8a0766c1f3
Shreya <shreyagoel098@gmail.com>
2024-12-01 12:57:32 +0530

    Add TabList View

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,34 @@
 import React, { useState } from "react";
 import TaskModal from "./components/Modals";
+import TabList from "./components/TabList";
 
 function App() {
-  const [list, setList] = useState([]);
+  const [list, setList] = useState([
+    {
+      title: "Task 1",
+      description: "This is a task",
+      createdAt: new Date(),
+      dueDate: new Date(),
+      priority: "High",
+    },
+    {
+      title: "Task 2",
+      description: "This is a task",
+      createdAt: new Date(),
+      dueDate: new Date(),
+      priority: "Medium",
+    },
+    {
+      title: "Task 3",
+      description: "This is a task",
+      createdAt: new Date(),
+      dueDate: new Date(),
+      priority: "Low",
+    },
+  ]);
   return (
     <div>
+      <TabList list={list} />
       <TaskModal title="+" id={0} createdAt={new Date()} />
     </div>
   );
---
+++ src/components/TabList.jsx
@@ -0,0 +1,60 @@
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import RemoveRedEyeIcon from "@mui/icons-material/RemoveRedEye";
+import DeleteIcon from "@mui/icons-material/Delete";
+import EditIcon from "@mui/icons-material/Edit";
+
+import React from "react";
+import TaskModal from "./Modals";
+
+function TabList({ list }) {
+  const handleAction = (action, id) => {
+    switch (action) {
+      case "edit":
+        return <TaskModal title="Edit" id={id} />;
+        break;
+      case "delete":
+        alert("Deleting " + id);
+        break;
+      case "read":
+        alert("Reading " + id);
+        break;
+      default:
+        break;
+    }
+  };
+  return (
+    <div>
+      {list.map((item, index) => {
+        return (
+          <List key={index}>
+            <ListItem>
+              <ListItemText primary={item.title} />
+              <ListItemText primary={item.priority} />
+              <ListItemText primary={item.createdAt.toString()} />
+              <ListItemText primary={item.dueDate.toString()} />
+              <ListItemButton>
+                <ListItemIcon>
+                  <EditIcon onClick={() => handleAction("edit", index)} />
+                </ListItemIcon>
+                <ListItemIcon>
+                  <DeleteIcon onClick={() => handleAction("delete", index)} />
+                </ListItemIcon>
+                <ListItemIcon>
+                  <RemoveRedEyeIcon
+                    onClick={() => handleAction("read", index)}
+                  />
+                </ListItemIcon>
+              </ListItemButton>
+            </ListItem>
+          </List>
+        );
+      })}
+    </div>
+  );
+}
+
+export default TabList;
